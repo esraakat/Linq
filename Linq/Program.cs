e6b423e34dde8329c8aafe13542c9d90a87d669a
esra akat <383196@ogr.ktu.edu.tr>
2021-03-03 20:53:15 +0300

    Add project files.

---
+++ Linq/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Category> categories = new List<Category>
+            {
+                 new Category { CategoryId = 1, CategoryName = "Computer"},
+                 new Category { CategoryId = 2, CategoryName = "Phone" },
+            };
+
+            List<Product> products = new List<Product>
+            {
+                new Product { ProductId = 1, CategoryId = 1, ProductName = "Acer Laptop", QuantityPerUnit = "32 GB Ram", UnitPrice = 10000, UnitInStock = 5},
+                new Product { ProductId = 2, CategoryId = 1, ProductName = "Asus Laptop", QuantityPerUnit = "16 GB Ram", UnitPrice = 8000, UnitInStock = 3},
+                new Product { ProductId = 3, CategoryId = 1, ProductName = "HP Laptop", QuantityPerUnit = "8 GB Ram", UnitPrice = 6000, UnitInStock = 2},
+                new Product { ProductId = 4, CategoryId = 2, ProductName = "Sampsung Phone", QuantityPerUnit = "4 GB Ram", UnitPrice = 5000, UnitInStock = 15},
+                new Product { ProductId = 5, CategoryId = 2, ProductName = "Apple Phone", QuantityPerUnit = "4 GB Ram", UnitPrice = 8000, UnitInStock = 0},
+            };
+
+            WhereTest(products);
+            GetProducts(products);
+            GetProductsLinq(products);
+            AnyTest(products);
+            FindTest(products);
+            FindAllTest(products);
+            AscDescTest(products);
+            ClassicLinqTest(products);
+            JoinTest(categories, products);
+
+        }
+
+        private static void JoinTest(List<Category> categories, List<Product> products)
+        {
+            var result = from p in products
+                         join c in categories
+                         on p.CategoryId equals c.CategoryId
+                         select new ProductDto { ProductId = p.ProductId, CategoryName = c.CategoryName, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName + ": " + product.CategoryName);
+            }
+        }
+
+        private static void ClassicLinqTest(List<Product> products)
+        {
+            var result = from p in products //alternative writing
+                         where p.UnitPrice > 6000
+                         orderby p.UnitPrice descending, p.ProductName ascending
+                         select new ProductDto { ProductId = p.ProductId, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+            
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void AscDescTest(List<Product> products)
+        {
+            //Single Line query
+            var result = products.Where(p => p.ProductName.Contains("top")).OrderByDescending(p => p.UnitPrice).ThenByDescending(p => p.ProductName);
+            
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void FindAllTest(List<Product> products)
+        {
+            var result = products.FindAll(p => p.ProductName.Contains("top")); 
+            Console.WriteLine(result.Count);
+        }
+
+        private static void FindTest(List<Product> products)
+        {
+            var result = products.Find(p => p.ProductId == 3);
+            Console.WriteLine(result.ProductName);
+        }
+
+        private static void WhereTest(List<Product> products)
+        {
+            Console.WriteLine("Algoritmic-------------");   //without linq
+
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 5000 & product.UnitInStock > 3)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+            }
+
+
+            Console.WriteLine("Linq-------------------");
+
+            var result = products.Where(p => p.UnitPrice > 5000 & p.UnitInStock > 3);
+            
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void AnyTest(List<Product> products)
+        {
+            var result = products.Any(p => p.ProductName == "Acer Laptop"); //if it empty
+            Console.WriteLine(result);
+        }
+
+        static List<Product> GetProducts(List<Product> products)  //without linq
+        {
+            List<Product> filteredProducts = new List<Product>();
+
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 5000 & product.UnitInStock > 3)
+                {
+                    filteredProducts.Add(product);
+                }
+            }
+            return filteredProducts;
+        }
+
+        static List<Product> GetProductsLinq(List<Product> products)
+        {
+            return products.Where(p => p.UnitPrice > 5000 && p.UnitInStock > 3).ToList();
+        }
+
+        class ProductDto
+        {
+            public int ProductId { get; set; }
+            public string CategoryName { get; set; }
+            public string ProductName { get; set; }
+            public decimal UnitPrice { get; set; }
+        }
+
+        class Product
+        {
+            public int ProductId { get; set; }
+            public int CategoryId { get; set; }
+            public string ProductName { get; set; }
+            public string QuantityPerUnit { get; set; }
+            public decimal UnitPrice { get; set; }
+            public int UnitInStock { get; set; }
+        }
+
+        class Category
+        {
+            public int CategoryId { get; set; }
+            public string CategoryName { get; set; }
+        }
+    }
+}
+//Use generally find, findallor where, any in real project
+//dto 
